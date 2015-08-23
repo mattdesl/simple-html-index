24d175b4c337c3c29441af6db6ab12f5c8153a74
Yoshua Wuyts <i@yoshuawuyts.com>
2015-08-24 02:20:51 +1000

    html: add optional css tag

--- test.js
+++ test.js
@@ -3,10 +3,11 @@ var html = require('./')
 var concat = require('concat-stream')
 
 test('should write html', function (t) {
-  t.plan(3)
+  t.plan(4)
   run(t, undefined, '<!doctype html><head><meta charset="utf-8"></head><body></body></html>')
   run(t, { title: 'foo' }, '<!doctype html><head><title>foo</title><meta charset="utf-8"></head><body></body></html>')
   run(t, { title: 'foo', entry: 'blah.js' }, '<!doctype html><head><title>foo</title><meta charset="utf-8"></head><body><script src="blah.js"></script></body></html>')
+  run(t, { title: 'foo', css: 'bla.css' }, '<!doctype html><head><title>foo</title><meta charset="utf-8"><link rel="stylesheet" href="bla.css"></head><body></body></html>')
 })
 
 function run (t, opt, expected) {
