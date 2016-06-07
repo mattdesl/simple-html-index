de64e934cec974811d64512d47c5a11bc8709faf
Yoshua Wuyts <i@yoshuawuyts.com>
2016-06-08 00:22:20 +0200

    index: add missing h5bp tags
    allows media queries to work based on displayport width

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ module.exports = createHtml
 function createHtml (opt) {
   opt = opt || {}
   return fromString([
-    '<!doctype html>',
+    '<!DOCTYPE html>',
+    '<html lang="en-us">',
     '<head>',
     opt.title ? ('<title>' + opt.title + '</title>') : '',
     '<meta charset="utf-8">',
