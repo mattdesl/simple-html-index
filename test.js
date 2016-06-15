7052c625b2d634270e32700240e30dec72b0c9ae
tgfjt <tgfjt.mail@gmail.com>
2016-06-15 13:34:31 +0900

    fix test

--- test.js
+++ test.js
@@ -4,11 +4,11 @@ var concat = require('concat-stream')
 
 test('should write html', function (t) {
   t.plan(5)
-  run(t, undefined, '<!doctype html><head><meta charset="utf-8"></head><body></body></html>')
-  run(t, { title: 'foo' }, '<!doctype html><head><title>foo</title><meta charset="utf-8"></head><body></body></html>')
-  run(t, { title: 'foo', entry: 'blah.js' }, '<!doctype html><head><title>foo</title><meta charset="utf-8"></head><body><script src="blah.js"></script></body></html>')
-  run(t, { title: 'foo', css: 'bla.css' }, '<!doctype html><head><title>foo</title><meta charset="utf-8"><link rel="stylesheet" href="bla.css"></head><body></body></html>')
-  run(t, { title: 'foo', favicon: true }, '<!doctype html><head><title>foo</title><meta charset="utf-8"><link rel="shortcut icon"type="image/x-icon" href="data:image/x-icon;,"></head><body></body></html>')
+  run(t, undefined, '<!DOCTYPE html><html lang="en-us"><head><meta charset="utf-8"></head><body></body></html>')
+  run(t, { title: 'foo' }, '<!DOCTYPE html><html lang="en-us"><head><title>foo</title><meta charset="utf-8"></head><body></body></html>')
+  run(t, { title: 'foo', entry: 'blah.js' }, '<!DOCTYPE html><html lang="en-us"><head><title>foo</title><meta charset="utf-8"></head><body><script src="blah.js"></script></body></html>')
+  run(t, { title: 'foo', css: 'bla.css' }, '<!DOCTYPE html><html lang="en-us"><head><title>foo</title><meta charset="utf-8"><link rel="stylesheet" href="bla.css"></head><body></body></html>')
+  run(t, { title: 'foo', favicon: true }, '<!DOCTYPE html><html lang="en-us"><head><title>foo</title><meta charset="utf-8"><link rel="shortcut icon"type="image/x-icon" href="data:image/x-icon;,"></head><body></body></html>')
 })
 
 function run (t, opt, expected) {
