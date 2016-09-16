db9570426f5e7247c9ab771380f893a57df4ffaa
Maximilian Antoni <mail@maxantoni.de>
2016-09-16 17:19:03 +0200

    Fix tests by adding missing dir attributes

--- test.js
+++ test.js
@@ -4,14 +4,14 @@ var concat = require('concat-stream')
 
 test('should write html', function (t) {
   t.plan(8)
-  run(t, undefined, '<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"></head><body></body></html>')
-  run(t, { title: 'foo' }, '<!DOCTYPE html><html lang="en"><head><title>foo</title><meta charset="utf-8"></head><body></body></html>')
-  run(t, { title: 'foo', base: '/' }, '<!DOCTYPE html><html lang="en"><head><title>foo</title><meta charset="utf-8"><base href="/"></head><body></body></html>')
-  run(t, { title: 'foo', entry: 'blah.js' }, '<!DOCTYPE html><html lang="en"><head><title>foo</title><meta charset="utf-8"></head><body><script src="blah.js"></script></body></html>')
-  run(t, { title: 'foo', css: 'bla.css' }, '<!DOCTYPE html><html lang="en"><head><title>foo</title><meta charset="utf-8"><link rel="stylesheet" href="bla.css"></head><body></body></html>')
-  run(t, { title: 'foo', favicon: true }, '<!DOCTYPE html><html lang="en"><head><title>foo</title><meta charset="utf-8"><link rel="shortcut icon"type="image/x-icon" href="data:image/x-icon;,"></head><body></body></html>')
-  run(t, { title: 'foo', favicon: true, lang: 'en-US' }, '<!DOCTYPE html><html lang="en-US"><head><title>foo</title><meta charset="utf-8"><link rel="shortcut icon"type="image/x-icon" href="data:image/x-icon;,"></head><body></body></html>')
-  run(t, { title: 'foo', favicon: true, lang: 'en-US', base: '/foo/' }, '<!DOCTYPE html><html lang="en-US"><head><title>foo</title><meta charset="utf-8"><base href="/foo/"><link rel="shortcut icon"type="image/x-icon" href="data:image/x-icon;,"></head><body></body></html>')
+  run(t, undefined, '<!DOCTYPE html><html lang="en" dir="ltr"><head><meta charset="utf-8"></head><body></body></html>')
+  run(t, { title: 'foo' }, '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>foo</title><meta charset="utf-8"></head><body></body></html>')
+  run(t, { title: 'foo', base: '/' }, '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>foo</title><meta charset="utf-8"><base href="/"></head><body></body></html>')
+  run(t, { title: 'foo', entry: 'blah.js' }, '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>foo</title><meta charset="utf-8"></head><body><script src="blah.js"></script></body></html>')
+  run(t, { title: 'foo', css: 'bla.css' }, '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>foo</title><meta charset="utf-8"><link rel="stylesheet" href="bla.css"></head><body></body></html>')
+  run(t, { title: 'foo', favicon: true }, '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>foo</title><meta charset="utf-8"><link rel="shortcut icon"type="image/x-icon" href="data:image/x-icon;,"></head><body></body></html>')
+  run(t, { title: 'foo', favicon: true, lang: 'en-US' }, '<!DOCTYPE html><html lang="en-US" dir="ltr"><head><title>foo</title><meta charset="utf-8"><link rel="shortcut icon"type="image/x-icon" href="data:image/x-icon;,"></head><body></body></html>')
+  run(t, { title: 'foo', favicon: true, lang: 'en-US', base: '/foo/' }, '<!DOCTYPE html><html lang="en-US" dir="ltr"><head><title>foo</title><meta charset="utf-8"><base href="/foo/"><link rel="shortcut icon"type="image/x-icon" href="data:image/x-icon;,"></head><body></body></html>')
 })
 
 function run (t, opt, expected) {
